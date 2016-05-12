7916c6775212335c54cbb3be3b9aaff463ab12c9
Bernard Helyer <b.helyer@gmail.com>
2016-05-12 21:23:59 +1200

    semantic: Expand lifter code.

--- src/volt/semantic/lifter.d
+++ src/volt/semantic/lifter.d
@@ -3,10 +3,13 @@
 module volt.semantic.lifter;
 
 import ir = volt.ir.ir;
+import ircopy = volt.ir.copy;
+import volt.ir.util;
 
 import volt.interfaces;
+import volt.errors;
 
-	
+
 /**
  * IR Lifter, aka Liftertron3000, copies and does transformations on IR.
  *
@@ -57,6 +60,9 @@ public:
 	ir.Module newModule()
 	{
 		mMod = new ir.Module();
+		mMod.name = buildQualifiedName(mMod.location, "CTFETESTMODULE");
+		mMod.children = new ir.TopLevelBlock();
+		mMod.children.location = mMod.location;
 		mMods ~= mMod;
 
 		return mMod;
@@ -84,7 +90,7 @@ protected:
 	 * The body, in and out contracts are left null and will need to be
 	 * copied by the caller. Intended to be used as a helper function.
 	 */
-	ir.Function copyDeclaration(ir.Function old)
+	ir.Function copyFunction(ir.Function old)
 	{
 		assert((old.uniqueId in mStore) is null);
 
@@ -94,10 +100,225 @@ protected:
 
 		auto func = new ir.Function();
 		func.location = old.location;
-		// TODO more fields
-
+		func.isResolved = old.isResolved;
+		func.isActualized = old.isActualized;
+		func.access = old.access;
+		func.myScope = copyScope(old.myScope, mMod.myScope, func);
+		func.kind = old.kind;
+		func.type = new ir.FunctionType(old.type);
+		version (Volt) {
+			func.params = new old.params[0 .. $];
+			func.nestedFunctions = new old.nestedFunctions[0 .. $];
+			func.scopeSuccesses = new old.scopeSuccesses[0 .. $];
+			func.scopeExits = new old.scopeExits[0 .. $];
+			func.scopeFailures = new old.scopeFailures[0 .. $];
+		} else {
+			func.params = old.params.dup;
+			func.nestedFunctions = old.nestedFunctions.dup;
+			func.scopeSuccesses = old.scopeSuccesses.dup;
+			func.scopeExits = old.scopeExits.dup;
+			func.scopeFailures = old.scopeFailures.dup;
+		}
+		foreach (i, param; old.params) {
+			func.params[i] = copyFunctionParam(param, func);
+		}
+		foreach (i, nfunc; old.nestedFunctions) {
+			func.nestedFunctions[i] = copyFunction(nfunc);
+		}
+		foreach (i, scopeFunc; old.scopeSuccesses) {
+			func.scopeSuccesses[i] = copyFunction(scopeFunc);
+		}
+		foreach (i, scopeFunc; old.scopeExits) {
+			func.scopeExits[i] = copyFunction(scopeFunc);
+		}
+		foreach (i, scopeFunc; old.scopeFailures) {
+			func.scopeExits[i] = copyFunction(scopeFunc);
+		}
+		func.name = old.name;
+		func.mangledName = old.mangledName;  // TODO: Prefix
+		func.suffix = old.suffix;
+		func.outParameter = old.outParameter;
+		if (old.inContract !is null) {
+			func.inContract = copyBlock(func.myScope, old.inContract);
+		}
+		if (old.outContract !is null) {
+			func.outContract = copyBlock(func.myScope, old.outContract);
+		}
+		if (old._body !is null) {
+			func._body = copyBlock(func.myScope, old._body);
+		}
+		if (old.thisHiddenParameter !is null) {
+			func.thisHiddenParameter = copyVariable(old.thisHiddenParameter);
+		}
+		if (old.nestedHiddenParameter !is null) {
+			func.nestedHiddenParameter = copyVariable(old.nestedHiddenParameter);
+		}
+		if (old.nestedVariable !is null) {
+			func.nestedVariable = copyVariable(old.nestedVariable);
+		}
+		if (func.nestStruct !is null) {
+			throw makeNotAvailableInCTFE(func, "nested functions");
+		}
+		func.isWeakLink = old.isWeakLink;
+		func.vtableIndex = old.vtableIndex;
+		func.loadDynamic = old.loadDynamic;
+		func.isMarkedOverride = old.isMarkedOverride;
+		func.isOverridingInterface = old.isOverridingInterface;
+		func.isAbstract = old.isAbstract;
+		func.isAutoReturn = old.isAutoReturn;
+		func.isLoweredScopeExit = old.isLoweredScopeExit;
+		func.isLoweredScopeFailure = old.isLoweredScopeFailure;
+		func.isLoweredScopeSuccess = old.isLoweredScopeSuccess;
 		mStore[old.uniqueId] = func;
+		mMod.children.nodes ~= func;
 		return func;
+	}
+
+	ir.Node copyNode(ir.Scope parent, ir.Node n)
+	{
+		switch (n.nodeType) with (ir.NodeType) {
+		case BlockStatement: assert(false);
+		case Alias: return copyAlias(cast(ir.Alias)n);
+		case FunctionParam: panicAssert(n, false); break;
+		case Variable: return copyVariable(cast(ir.Variable)n);
+		default: return ircopy.copyNode(n);  // TODO: Above nodes nested in these nodes.
+		}
+		assert(false);
+	}
+
+	ir.BlockStatement copyBlock(ir.Scope parent, ref ir.BlockStatement old)
+	{
+		assert(old !is null);
+		auto bs = new ir.BlockStatement();
+		version (Volt) {
+			bs.statements = new old.statements[0 .. $];
+		} else if (old.statements.length > 0) {
+			bs.statements = old.statements.dup;
+		}
+		bs.myScope = new ir.Scope();
+
+		foreach (ref n; bs.statements) {
+			copyNode(bs.myScope, n);
+		}
+
+		foreach (k, v; old.myScope.symbols) {
+			bs.myScope.symbols[k] = copyStore(parent, v);
+		}
+
+		return bs;
+	}
+
+	/**
+	 * Copy a scope. The owner is the _new_ Node that holds this scope.
+	 */
+	ir.Scope copyScope(ir.Scope old, ir.Scope parent, ir.Node owner)
+	{
+		auto _scope = new ir.Scope(mMod, old.name);  // TODO: CTFE prefix?
+		_scope.anon = old.anon;
+		_scope.node = owner;
+		if (old.parent !is null) {
+			auto oldMod = cast(ir.Module)old.parent.node;
+			if (oldMod !is null) {
+				_scope.parent = mMod.myScope;
+			} else {
+				assert(parent !is null);
+				_scope.parent = parent;
+			}
+		}
+		foreach (k, v; old.symbols) {
+			_scope.symbols[k] = copyStore(_scope, v);
+		}
+		panicAssert(owner, old.importedModules.length == 0);
+		panicAssert(owner, old.importedAccess.length == 0);
+		_scope.nestedDepth = old.nestedDepth;
+		return _scope;
+	}
+
+	ir.Store copyStore(ir.Scope parent, ir.Store old)
+	{
+		auto store = new ir.Store();
+		store.name = old.name;  // TODO: CTFE prefix?
+		store.parent = parent;
+		store.kind = old.kind;
+		store.node = copyNode(parent, old.node);
+		if (old.myScope !is null) {
+			//store.myScope = copyScope(old.myScope, parent, copyNode(old.myScope.node));
+			assert(false);
+		}
+		if (old.lookScope !is null) {
+			//store.lookScope = copyScope(old.lookScope, parent, copyNode(old.lookScope.node));
+			assert(false);
+		}
+		version (Volt) {
+			store.functions = new old.functions[0 .. $];
+			store.aliases = new old.aliases[0 .. $];
+		} else {
+			store.functions = old.functions.dup;
+			store.aliases = old.aliases.dup;
+		}
+		foreach (i, func; old.functions) {
+			store.functions[i] = copyFunction(func);
+		}
+		foreach (i, _alias; old.aliases) {
+			store.aliases[i] = copyAlias(_alias);
+		}
+		panicAssert(old.node, old.myAlias is null);
+		store.access = old.access;
+		return store;
+	}
+
+	ir.Alias copyAlias(ir.Alias old)
+	{
+		auto _alias = new ir.Alias();
+		_alias.location = old.location;
+		_alias.access = old.access;
+		_alias.name = old.name;
+		panicAssert(old, old.externAttr is null);
+		_alias.type = ircopy.copyType(old.type);
+		_alias.id = ircopy.copy(old.id);
+		if (old.store !is null) {
+			assert(false);
+		}
+//		_alias.store = copyStore(old.store, null);
+		panicAssert(old, old.templateInstance is null);
+		return _alias;
+	}
+
+	ir.FunctionParam copyFunctionParam(ir.FunctionParam old, ir.Function newFunction)
+	{
+		auto fparam = new ir.FunctionParam();
+		fparam.location = old.location;
+		fparam.func = newFunction;
+		fparam.index = old.index;
+		if (old.assign !is null) {
+			fparam.assign = ircopy.copyExp(old.assign);
+		}
+		fparam.name = old.name;
+		fparam.hasBeenNested = old.hasBeenNested;
+		return fparam;
+	}
+
+	ir.Variable copyVariable(ir.Variable old)
+	{
+		auto var = new ir.Variable();
+		var.location = old.location;
+		var.isResolved = old.isResolved;
+		var.access = old.access;
+		var.type = ircopy.copyType(old.type);
+		var.name = old.name;
+		var.mangledName = old.mangledName;  // TODO: Remangle?
+		if (old.assign !is null) {
+			var.assign = ircopy.copyExp(old.assign);
+		}
+		var.storage = old.storage;
+		var.linkage = old.linkage;
+		var.isWeakLink = old.isWeakLink;
+		var.isExtern = old.isExtern;
+		var.isOut = old.isOut;
+		var.hasBeenDeclared = old.hasBeenDeclared;
+		var.useBaseStorage = old.useBaseStorage;
+		var.specialInitValue = old.specialInitValue;
+		return var;
 	}
 
 	/**
@@ -121,10 +342,20 @@ protected:
 	override ir.Function doLift(ir.Function old)
 	{
 		// Copy declaration and add function to store.
-		auto func = copyDeclaration(old);
+		auto func = copyFunction(old);
 
 		// TODO copy in, out and body.
 
+		return func;
+	}
+
+	override ir.Function copyFunction(ir.Function old)
+	{
+		if (old.kind != ir.Function.Kind.Function) {
+			throw makeNotAvailableInCTFE(old, "non toplevel functions");
+		}
+		auto func = super.copyFunction(old);
+		func.mangledName = "__CTFE_" ~ func.mangledName;
 		return func;
 	}
 }
